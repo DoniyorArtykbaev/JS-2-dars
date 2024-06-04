34329069edcc0f45b37159ef65aad726845d84e9
DoniyorArtykbaev <donerartykbaev@gmail.com>
2024-06-04 08:21:33 +0500

    JS 2-dars
    Zeromax IT-Center 35-guruh

---
+++ JS/main.js
@@ -0,0 +1,121 @@
+
+
+
+// if 
+// else
+// else if    =>
+
+//if("salom" == "salom"){
+//    console.log("Ular teng!");
+///}
+//else{
+//    console.log("Ular teng emas!")
+//}
+
+//if(45 >= 45){
+ //   console.log("Ular teng yoki katta!")
+//}
+//else{
+//    console.log("Ular teng emas yoki katta emas!")
+//}
+
+//if("Salom" == "Salom"){
+//    console.log("Ular teng!")
+//}
+
+//else{
+//    console.log("Ular teng emas!")
+//}
+
+//if("45*2+10 >=100"){
+//    console.log("Ular teng yoki katta!")
+//}
+
+//else{
+//    console.log("Ular teng emas yoki katta!")
+//}
+
+//if (45 <= 45){
+//    console.log("Solishtilayotgan son 45ga teng yoki katta!")
+//}
+//else{
+//    console.log("Solishtilayotgan son 45ga teng yoki katta!")
+//}
+
+if(45 === "45"){
+    console.log("Ular teng")
+}
+else{
+    console.log("Ular teng emas!")
+}
+
+
+//if(45 == 44){
+//    console.log("1-shart bajarildi ")
+//}
+
+//else if(45 = 46){
+//    console.log("2-shart bajarildi!")
+//}
+//else{
+//    console.log("Yuqoridagi shartlardan birortasi bajarilmadi!")
+//}
+
+
+//let a = 45;
+//let b = 45;
+
+//if(a == b){
+//    console.log("a berilgan")
+//}
+//else{
+//    console.log("Ular teng emas!")
+//}
+
+//let a = 46;
+//let b = 10;
+//let c = 19;
+//let d = 49;
+//if(a == 45 && b == 11 && c == 16 && d == 45){
+//    console.log("-----");
+//}
+//else{
+ //   console.log("_____")
+//}
+//if(a == 35 && b == 31 && c == 46 && d == 55){
+//    console.log("Amalar tog'ri bajarildi");
+//}
+//else(a == 35 && b == 31 && c == 46 && d == 55){
+//    console.log("Amalar tog'ri bajarildi");
+//}
+
+if(a == 46 || b == 10 || c == 19 || d == 49){
+    console.log("-----");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
